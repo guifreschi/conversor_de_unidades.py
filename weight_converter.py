4d7e026f08ced824ce0017e7c87e5efc5c14b78f
Guilherme <guilhermefreschix@gmail.com>
2024-09-30 13:38:38 -0300

    Adiciona conversor de peso

---
+++ weight_converter.py
@@ -0,0 +1,17 @@
+def gram_to_kilogram(gram):
+    return gram / 1000
+
+def kilogram_to_gram(kilogram):
+    return kilogram * 1000
+
+def kilogram_to_pound(kilogram):
+    return kilogram * 2.20462
+
+def pound_to_kilogram(pound):
+    return pound / 2.20462
+
+def gram_to_pound(gram):
+    return gram / 453.592
+
+def pound_to_gram(pound):
+    return pound * 453.592
