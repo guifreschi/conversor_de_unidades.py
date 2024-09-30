293a63a78b40bd673c053821a00c3d200742b766
Guilherme <guilhermefreschix@gmail.com>
2024-09-30 14:26:25 -0300

    Adiciona conversor de distâncias

---
+++ distance_converter.py
@@ -0,0 +1,35 @@
+def meter_to_kilometer(meter):
+    return meter / 1000
+
+def meter_to_mile(meter):
+    return meter / 1609.34
+
+def meter_to_feet(meter):
+    return meter * 3.28084
+
+def kilometer_to_meter(kilometer):
+    return kilometer * 1000
+
+def kilometer_to_mile(kilometer):
+    return kilometer / 1.60934
+
+def kilometer_to_feet(kilometer):
+    return kilometer * 3280.84
+
+def mile_to_meter(mile):
+    return mile * 1609.34
+
+def mile_to_kilometer(mile):
+    return mile * 1.60934
+
+def mile_to_feet(mile):
+    return mile * 5280
+
+def feet_to_meter(feet):
+    return feet / 3.28084
+
+def feet_to_kilometer(feet):
+    return feet / 3280.84
+
+def feet_to_mile(feet):
+    return feet / 5280
